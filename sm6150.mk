168fb4a11477850b1a5263244ad29ead53b0d242
PeterCxy <peter@typeblog.net>
2021-09-23 19:10:02 +0530

    sm6150-common: Add missing OMX stack libstagefright-omx & android.hardware.media.omx@1.0-service
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Prince Cooper <pcooper7736@gmail.com>
    Change-Id: I17f9ca075ebe6b503f3b34a3af7df8aaee02dc0e

--- sm6150.mk
+++ sm6150.mk
@@ -233,6 +233,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libarbitrarybytes \
@@ -249,7 +250,8 @@ PRODUCT_PACKAGES += \
     libOmxSwVencMpeg4 \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_omx.vendor
 
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
